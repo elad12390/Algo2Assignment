494b04d634d9954ec452a53fbcc9adc80134a14b
אלעד <elad12390@gmail.com>
2021-04-05 03:14:45 +0300

    Changed naming of graph to MatrixGraph Created by kinda the same code a list graph (std::vector)
    learned some std::async operations for using threads when testing
    (because we need to test 10 times for each graph)
    running 10 threads will be the most reasonable choice.
    maybe more if we decide to.

--- Main.cpp
+++ Main.cpp
@@ -1,43 +1,77 @@
 #include <iostream>
+#include <string>
 #include <vector>
-#include<string>
+#include <chrono>
+#include <future>
 
-class Graph {
+using namespace std;
+using namespace std::chrono;
+
+template <class T>
+using vector2D = vector<vector<T>*>;
+
+
+class ListGraph {
 public:
-	bool** adjacencyMatrix; 
-	int size;
+	vector2D<int> adj;
+	int vertex_count = 0;
 
-	Graph(int vertexNumber) {
-		this->size = vertexNumber;
-		adjacencyMatrix = new bool*[vertexNumber];
-		for (size_t i = 0; i < vertexNumber; i++)
-			adjacencyMatrix[i] = new bool[vertexNumber]();
+	// O(v)
+	ListGraph(int v) {
+		this->vertex_count = v;
+		for (size_t i = 0; i < this->vertex_count; i++)
+		{
+			this->adj.push_back(new vector<int>(this->vertex_count));
+		}
 	}
+
+	~ListGraph() {
+		for (size_t i = 0; i < adj.size(); i++)
+		{
+			this->adj.at(i)->clear();
+			delete this->adj.at(i);
+		}
+		this->adj.clear();
+	}
+
+	// O(1)
+	ListGraph& addEdge(int source, int destination) {
+		this->adj.at(source)->push_back(destination);
+		this->adj.at(destination)->push_back(source);
+		return *this;
+	}
+
+	// takes the lower half of the matrix and inserts it (which means (V^2)/2 - V) => O(V^2)
+	ListGraph& randomize(float p) {
+		for (size_t i = 1; i < this->vertex_count; i++)
+			for (size_t j = 0; j < i; j++) {
+
+				//generate random number between 0 to 1. If the number is smaller than p, we add the edge.
+				if (((float)rand() / RAND_MAX) < p) {
+					this->addEdge(i, j);
+				}
+			}
+
+		return *this;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const ListGraph& dt);
 
 	
-	Graph* randomize(float p) { 
-		srand(time(NULL));
-		for (size_t i = 1; i < this->size; i++)
-			for (size_t j = 0; j < i; j++)						
-				//generate random number between 0 to 1. If the number is smaller than p, we add the edge.
-				adjacencyMatrix[j][i] = adjacencyMatrix[i][j] = ((float)rand()/RAND_MAX) < p; 
-
-		return this;
-	}
-
-	friend std::ostream& operator<<(std::ostream& os, const Graph& dt);
 };
 
 
 
-std::ostream& operator<<(std::ostream& os, const Graph& graph)
+std::ostream& operator<<(std::ostream& os, const ListGraph& graph)
 {
-	for (size_t i = 0; i < graph.size; i++)
+	for (size_t i = 0; i < graph.vertex_count; i++)
 	{
+		int vec_size = graph.adj.at(i)->size();
+
 		os << "[";
-		for (size_t j = 0; j < graph.size; j++) {
-			os << graph.adjacencyMatrix[i][j];
-			if (j < (graph.size - 1))
+		for (size_t j = 0; j < vec_size; j++) {
+			os << graph.adj.at(i)->at(j);
+			if (j < (vec_size - 1))
 				os << ",";
 		}
 		os << "]" << std::endl;
@@ -45,16 +79,32 @@ std::ostream& operator<<(std::ostream& os, const Graph& graph)
 	return os;
 }
 
-Graph* build_random_graph(int v, float p) {
-	Graph* g = new Graph(v);
-	g->randomize(p);
-	return g;
+void run_test(double p) {
+	auto g = ListGraph(1000);
+	g.randomize(p);
 }
 
-
 int main() {
-	Graph g = Graph(5);
-	g.randomize(.3);
-	std::cout << g;
+	srand(time(NULL));
+
+	cout << "Starting..." << endl;
+
+
+	for (int i = 0; i < 500; i += 1) {
+		auto start = high_resolution_clock::now();
+
+		auto task1 = std::async(run_test, 0.1);
+		auto task2 = std::async(run_test, 0.2);
+		auto task3 = std::async(run_test, 0.3);
+		auto task4 = std::async(run_test, 0.4);
+		auto task5 = std::async(run_test, 0.5);
+		auto task6 = std::async(run_test, 0.6);
+		auto task7 = std::async(run_test, 0.7);
+		auto task8 = std::async(run_test, 0.8);
+		auto task9 = std::async(run_test, 0.9);
+
+		auto stop = high_resolution_clock::now();
+		cout << "finished test number " << i << " at " << duration_cast<microseconds>(stop - start).count() << " microseconds" << endl;
+	}
 	return 0;
-} 
+}
---
+++ MatrixGraph.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+class MatrixGraph {
+public:
+	bool** adjacencyMatrix; 
+	int vertex_count;
+
+	MatrixGraph(int vertex_count) {
+		this->vertex_count = vertex_count;
+		adjacencyMatrix = new bool*[vertex_count];
+		for (size_t i = 0; i < vertex_count; i++)
+			adjacencyMatrix[i] = new bool[vertex_count]();
+	}
+
+	
+	MatrixGraph* randomize(float p) {
+		srand(time(NULL));
+		for (size_t i = 1; i < this->vertex_count; i++)
+			for (size_t j = 0; j < i; j++) {
+				//generate random number between 0 to 1. If the number is smaller than p, we add the edge.
+				adjacencyMatrix[j][i] = adjacencyMatrix[i][j] = ((float)rand() / RAND_MAX) < p;
+			}
+
+		return this;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const MatrixGraph& dt);
+};
+
+
+
+std::ostream& operator<<(std::ostream& os, const MatrixGraph& graph)
+{
+	for (size_t i = 0; i < graph.vertex_count; i++)
+	{
+		os << "[";
+		for (size_t j = 0; j < graph.vertex_count; j++) {
+			os << graph.adjacencyMatrix[i][j];
+			if (j < (graph.vertex_count - 1))
+				os << ",";
+		}
+		os << "]" << std::endl;
+	}
+	return os;
+}
+
+MatrixGraph* build_random_graph(int v, float p) {
+	MatrixGraph* g = new MatrixGraph(v);
+	g->randomize(p);
+	return g;
+}
